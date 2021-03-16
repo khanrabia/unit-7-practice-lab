75e7e02826e6cf18bbf6a36c4ee671dc53897028
khanrabia <khan.rab34@gmail.com>
2021-03-16 16:05:35 -0400

    COMPLETED

--- lesson-0-practice-lab/server.js
+++ lesson-0-practice-lab/server.js
@@ -3,8 +3,8 @@ let hostName = "127.0.0.1";
 let port = 8000;
 
 const server = http.createServer((request, response) => {
-	response.writeHead(200);
-	response.end('Finally, I did it!');
+	response.writeHead(200, { 'Content-Type': 'HTML' });
+	response.end('<h1>Finally, I did it!</h1>');
 });
 
 server.listen(port, hostName, () => {
