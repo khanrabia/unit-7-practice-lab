f56d951c994c137dbe9dccfea8c2db420555b264
khanrabia <khan.rab34@gmail.com>
2021-03-16 15:36:33 -0400

    Completed

--- lesson-0-practice-lab/server.js
+++ lesson-0-practice-lab/server.js
@@ -1,11 +1,10 @@
-let http = require('http');
-let hostName = '127.0.0.1';
+let http = require("http");
+let hostName = "127.0.0.1";
 let port = 8000;
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.writeHead('Content-Type', 'text/plain');
-	res.end('Hello World!');
+const server = http.createServer((request, response) => {
+	response.writeHead(200);
+	response.end('Finally, I did it!');
 });
 
 server.listen(port, hostName, () => {
